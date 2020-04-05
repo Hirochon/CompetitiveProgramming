88a1d433695402d1c07191f5ae6954430448fbcf
Hirochon <heacet43@gmail.com>
2020-04-05 12:35:17 +0900

    [mod] D.cを修正

--- ABC161/D.c
+++ ABC161/D.c
@@ -3,7 +3,7 @@
 
 int main(void){
     int K, flag, flag2, len;
-    unsigned long int i;
+    int i, j;
     char mozi[50];
     scanf("%d", &K);
     if (K <= 12){
@@ -18,18 +18,18 @@ int main(void){
             flag2 = 0;
             for (j = 0; j < len-1; j++){
                 if ((mozi[j] == mozi[j+1]) || (mozi[j]+1 == mozi[j+1]) || (mozi[j]-1 == mozi[j+1])){
-                    flag2 += 2;
+                    flag2 += 1;
                     continue;
                 }
                 else{
                     break;
                 }
             }
-            if (flag2 == (len - 1)){
+            if (flag2 == len-1){
                 flag += 1;
             }
         }
-        printf(i)
+        printf("%d\n", i);
     }
     return 0;
 }
