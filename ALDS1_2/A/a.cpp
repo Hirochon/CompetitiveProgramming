c97f2e6c2cf30e884afcf6c8b5b590ac03d6a1c9
Hirochon <heacet43@gmail.com>
2021-08-04 23:04:46 +0000

    [add] バブルソートを解いた

---
+++ ALDS1_2/A/a.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+static const int MAX = 100;
+
+void callList(int *A, int N) {
+    for (int i = 0; i < N; i++) {
+        if (i != N-1) {
+            printf("%d ", A[i]);
+        }
+        else {
+            printf("%d\n", A[i]);
+        }
+    }
+}
+
+int bubbleSort(int *A, int N) {
+    int swapNum = 0;
+    int tmp;
+    for (int j = N-1; j > 0; j--) {
+        for (int i = N-1; i > N-j-1; i--) {
+            if (A[i] < A[i-1]) {
+                tmp = A[i];
+                A[i] = A[i-1];
+                A[i-1] = tmp;
+                swapNum++;
+            }
+        }
+    }
+    return swapNum;
+}
+
+int main() {
+    int N, A[MAX], swapNum;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    swapNum = bubbleSort(A, N);
+    callList(A, N);
+    cout << swapNum << endl;
+    return 0;
+}
