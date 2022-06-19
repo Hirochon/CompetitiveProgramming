51c4de2f09350e3c7a9311076e2dfb06de1c8f36
Hirochon <heacet43@gmail.com>
2022-06-19 15:13:10 +0900

    update: とけた

--- abc/ABC256/d2.py
+++ abc/ABC256/d2.py
@@ -1,15 +1,27 @@
-from ctypes import HRESULT
-import heapq
-
 N = int(input())
-L = []
-R = []
-heapq.heapify(L)
-heapq.heapify(R)
+MAX_NUM = 200100
+LR = [0 for _ in range(MAX_NUM)]
+
 for i in range(N):
     l, r = map(int, input().split())
-    heapq.heappush(L, l)
-    heapq.heappush(R, r)
+    LR[l] += 1
+    LR[r] -= 1
 
-while L.__len__():
-    
+# print(LR[:100])
+
+ans = []
+start = 0
+s_point = 0
+for i, lr in enumerate(LR):
+    if lr > 0:
+        s_point += lr
+        if start == 0:
+            start = i
+    elif lr < 0:
+        s_point += lr
+        if s_point == 0:
+            ans.append([start, i])
+            start = 0
+
+for a in ans:
+    print(a[0], a[1])
