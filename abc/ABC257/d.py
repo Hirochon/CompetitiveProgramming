aad1226e38b0c71260e29396a13b44ad758f15e5
Hirochon <heacet43@gmail.com>
2022-06-26 07:00:34 +0900

    add: d

--- abc/ABC257/d.py
+++ abc/ABC257/d.py
@@ -4,24 +4,29 @@ N = int(input())
 X = []
 Y = []
 P = []
-for i in range(N):
+for _ in range(N):
     x, y, p = map(int, input().split())
     X.append(x)
     Y.append(y)
     P.append(p)
 
-nums = []
+XY = [[0 for _ in range(N)] for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        x = abs(X[i] - X[j])
+        y = abs(Y[i] - Y[j])
+        XY[i][j] = math.ceil((x + y) / P[i])
+
+# print(XY)
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            XY[i][j] = min(XY[i][j], max(XY[i][k], XY[k][j]))
+
+# print(XY)
+
 ans = 5000000000
 for i in range(N):
-    max_num = 0
-    for j in range(N):
-        if i == j:
-            continue
-        x_num = abs(X[i] - X[j])
-        y_num = abs(Y[i] - Y[j])
-        max_num = max(max_num, x_num + y_num)
-    print("max_num", max_num)
-    value = math.ceil(float(max_num) / float(P[i]))
-    print("value", value)
-    ans = min(ans, value)
+    ans = min(ans, max(XY[i]))
 print(ans)
